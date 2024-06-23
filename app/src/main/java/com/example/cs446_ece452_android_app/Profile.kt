3918f55a9e92af5781f35f7f9de846b6764e67be
felix <felixliu135@gmail.com>
2024-06-23 17:32:09 -0400

    profile page

--- app/src/main/java/com/example/cs446_ece452_android_app/Profile.kt
+++ app/src/main/java/com/example/cs446_ece452_android_app/Profile.kt
@@ -1,4 +1,19 @@
 package com.example.cs446_ece452_android_app
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.ComponentActivity
 
-class Profile {
+class Profile : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.profile)
+
+        // Set user details
+        val username: TextView = findViewById(R.id.username)
+        val email: TextView = findViewById(R.id.email)
+        val address: TextView = findViewById(R.id.address)
+        username.text = "John Doe"
+        email.text = "JohnDoe@gmail.com"
+        address.text = "Japan"
+    }
 }
