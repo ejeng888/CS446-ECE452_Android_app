8011159a5fcfe9a9c6dbb7be8e6733cb0af1ea36
Martin Zhao <martinzha28@gmail.com>
2024-06-28 17:15:38 -0400

    more updates to nav bar

--- app/src/main/java/com/example/cs446_ece452_android_app/ui/components/BottomNavBar.kt
+++ app/src/main/java/com/example/cs446_ece452_android_app/ui/components/BottomNavBar.kt
@@ -1,6 +1,7 @@
 package com.example.cs446_ece452_android_app.ui.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.icons.Icons
@@ -23,11 +24,14 @@ import androidx.navigation.compose.rememberNavController
 import com.example.cs446_ece452_android_app.ui.theme.Blue1
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
+import com.example.cs446_ece452_android_app.ui.theme.Blue5
 import com.example.cs446_ece452_android_app.ui.theme.DarkBlue
 import com.example.cs446_ece452_android_app.ui.theme.Gray
 
@@ -49,37 +53,49 @@ fun BottomNavigationBar(navController: NavController) {
         BottomNavigationModel("Profile", "Profile", Icons.Default.Person, Icons.Outlined.Person)
     )
 
-    Row(
-        modifier = Modifier
-            .background(Blue1)
-            .fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceEvenly,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        NavList.forEach { item ->
+    Column {
 
-            val selected = item.route == backStackEntry.value?.destination?.route
-            val icon = if (selected) item.iconSelected else item.iconDeseleted
-            val displayColor = if (selected) DarkBlue else Gray
+        HorizontalDivider(
+            color = Gray,
+            modifier = Modifier
+                .height(1.5.dp)
+                .fillMaxWidth()
+        )
 
-            Column (
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier.padding(10.dp)
-            ) {
-                Icon(
-                    imageVector = icon,
-                    contentDescription = item.name,
-                    tint = displayColor,
-                    modifier = Modifier.size(32.dp)
-                )
-                Text(
-                    text = item.name,
-                    fontSize = 9.sp,
-                    textAlign = TextAlign.Center,
-                    color = displayColor
-                )
+        Row(
+            modifier = Modifier
+                .background(Blue1)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            NavList.forEach { item ->
+
+                val selected = item.route == backStackEntry.value?.destination?.route
+                val icon = if (selected) item.iconSelected else item.iconDeseleted
+                val displayColor = if (selected) DarkBlue else Blue5
+
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .padding(horizontal = 15.dp, vertical = 5.dp) // Modify this to make the clicked button circular
+                        .clickable { navController.navigate(item.route) }
+                ) {
+                    Icon(
+                        imageVector = icon,
+                        contentDescription = item.name,
+                        tint = displayColor,
+                        modifier = Modifier.size(38.dp))
+                    Text(
+                        text = item.name,
+                        fontSize = 9.sp,
+                        textAlign = TextAlign.Center,
+                        color = displayColor
+                    )
+                }
+
             }
-
         }
     }
 
