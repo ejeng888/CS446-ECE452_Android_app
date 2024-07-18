483b1375c2a0c89db60f72ce8caab56dc08b083e
Martin Zhao <martinzha28@gmail.com>
2024-07-18 00:39:11 -0400

    temporarily remove login

--- app/src/main/java/com/example/cs446_ece452_android_app/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/example/cs446_ece452_android_app/ui/screens/LoginScreen.kt
@@ -118,9 +118,9 @@ fun LoginScreen(navController : NavController) {
 
             Spacer(modifier = Modifier.height(30.dp))
             FilledButton(labelVal = "Login", navController = navController, function = {
-                // login(context = context, email = enteredEmail, password = enteredPassword, destination = "routes")
+                login(context = context, email = enteredEmail, password = enteredPassword, destination = "routes")
                 // For Richard, uncomment above line to actually log in, comment out next line
-                navController.navigate("routes")
+                // navController.navigate("routes")
             })
             Spacer(modifier = Modifier.height(10.dp))
             OutlinedButton(labelVal = "Create Account", navController = navController, destination = "Signup")
